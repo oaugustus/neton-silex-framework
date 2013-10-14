b271f4a0a3de1a5f4f3bdff08420094b6d21bd8f
Otavio Fernandes <otavio@neton.com.br>
2013-10-14 16:41:37 -0300

    Implementada função de geração de filtro customizado para grid do extjs

--- src/Neton/Silex/Framework/BaseStore.php
+++ src/Neton/Silex/Framework/BaseStore.php
@@ -229,6 +229,80 @@ class BaseStore extends BaseService
     }
 
     /**
+     * Aplica filtro baseado no GridFeature Filter.
+     *
+     * @param Array $filters
+     * @return string
+     */
+    protected function getGridFilter($filters)
+    {
+        // GridFilters sends filters as an Array if not json encoded
+        if (is_array($filters)) {
+            $encoded = false;
+        } else {
+            $encoded = true;
+            $filters = json_decode($filters);
+        }
+
+        $where = ' 0 = 0 ';
+        $qs = '';
+
+        // loop through filters sent by client
+        if (is_array($filters)) {
+            for ($i=0;$i<count($filters);$i++){
+                $filter = $filters[$i];
+
+                // assign filter data (location depends if encoded or not)
+                if ($encoded) {
+                    $field = $filter->field;
+                    $value = $filter->value;
+                    $compare = isset($filter->comparison) ? $filter->comparison : null;
+                    $filterType = $filter->type;
+                } else {
+                    $field = $filter['field'];
+                    $value = $filter['data']['value'];
+                    $compare = isset($filter['data']['comparison']) ? $filter['data']['comparison'] : null;
+                    $filterType = $filter['data']['type'];
+                }
+
+                switch($filterType){
+                    case 'string' : $qs .= " AND ".$field." LIKE '%".$value."%'"; Break;
+                    case 'list' :
+                        if (strstr($value,',')){
+                            $fi = explode(',',$value);
+                            for ($q=0;$q<count($fi);$q++){
+                                $fi[$q] = "'".$fi[$q]."'";
+                            }
+                            $value = implode(',',$fi);
+                            $qs .= " AND ".$field." IN (".$value.")";
+                        }else{
+                            $qs .= " AND ".$field." = '".$value."'";
+                        }
+                        Break;
+                    case 'boolean' : $qs .= " AND ".$field." = ".($value); Break;
+                    case 'numeric' :
+                        switch ($compare) {
+                            case 'eq' : $qs .= " AND ".$field." = ".$value; Break;
+                            case 'lt' : $qs .= " AND ".$field." < ".$value; Break;
+                            case 'gt' : $qs .= " AND ".$field." > ".$value; Break;
+                        }
+                        Break;
+                    case 'date' :
+                        switch ($compare) {
+                            case 'eq' : $qs .= " AND ".$field." = '".date('Y-m-d',strtotime($value))."'"; Break;
+                            case 'lt' : $qs .= " AND ".$field." < '".date('Y-m-d',strtotime($value))."'"; Break;
+                            case 'gt' : $qs .= " AND ".$field." > '".date('Y-m-d',strtotime($value))."'"; Break;
+                        }
+                        Break;
+                }
+            }
+            $where .= $qs;
+        }
+
+        return $where;
+    }
+
+    /**
      * Retorna os nomes das colunas da tabela.
      *
      * @return Array
